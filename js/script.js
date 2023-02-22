f0b0d9a1e9a9c05bc98dc165ebe2be07c036c447
aneekmahajan <aneek.mahajan@icloud.com>
2023-02-22 13:19:11 -0500

    button working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-// set of frame size
-const FRAME_HEIGHT = 500;
-const FRAME_WIDTH = 500; 
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
-
-
 // Set up the SVG container
 const svg = d3.select("#scatterplot")
   .append("svg")
@@ -36,29 +30,20 @@ d3.csv("data/scatter-data.csv").then(function(data) {
         // Onclick: https://stackoverflow.com/questions/22584060/d3js-how-to-toggle-css-class-after-clicking-on-anelement
         .on("click", function() { 
             d3.select(this).classed("addStroke", !d3.select(this).classed("addStroke"))
-        })
 
-        // deal with when a point is clicked
-        .on("click", function(d){
-          let last_clicked = d3.select(this).attr("id")
-          d3.select("#coord").text(last_clicked)
-          // Check if stroke exists, and change accordingly
-          if(d3.select(this).style('stroke') == 'orange'){
-              d3.select(this).style("stroke", "none");
-          }
-          else{
-              d3.select(this).style("stroke-width", "4px");
-              d3.select(this).style("stroke", "orange");
-          }})
+            // last clicked
+            let last_clicked = d3.select(this).attr("id")
+            d3.select("#coord").text("Last Point Clicked: (" + last_clicked + ")")
+        })
 
           // change color when point is hovered 
           .on("mouseover", function(d) {
-            d3.select(this).style("fill", "lightblue");
+            d3.select(this).style("fill", "#f5a742");
           })
 
           // change color when point is unhovered
           .on("mouseout", function(d) {
-            d3.select(this).style("fill", "black");
+            d3.select(this).style("fill", "#34baeb");
           });
 
     // https://d3-graph-gallery.com/intro_d3js.html
@@ -75,20 +60,110 @@ d3.csv("data/scatter-data.csv").then(function(data) {
 
 
     d3.selectAll("#butt")
-    .on("click", function(d){
+  .on("click", function(d){
     let xVal = document.getElementById("xCoord").value;
     let yVal = document.getElementById("yCoord").value;
-    svg.append('g')
-    .selectAll("dot")
+    console.log(xVal, yVal)
+    
+    // Create a new data object with the user-entered values
+    const newData = { x: xVal, y: yVal };
+    
+    // Append a new circle element to the SVG
+    svg.append("circle")
+      // Using datum for singular point
+      .datum(newData)
+      .attr("cx", d => xScale(d.x))
+      .attr("cy", d => yScale(d.y))
+      .attr("r", 10)
+      .attr("class", "point")
+      .attr("id", d => d.x + "," + d.y)
+      .on("click", function() { 
+        d3.select(this).classed("addStroke", !d3.select(this).classed("addStroke"))
+        let last_clicked = d3.select(this).attr("id")
+        d3.select("#coord").text("Last Point Clicked: (" + last_clicked + ")")
+      })
+      .on("mouseover", function(d) {
+        d3.select(this).style("fill", "#f5a742");
+      })
+      .on("mouseout", function(d) {
+        d3.select(this).style("fill", "#34baeb");
+      });
+  });
+});
+
+const svg_Bar = d3.select("#barchart")
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 500);
+
+d3.csv("data/bar-data.csv").then(function(data) {
+  // Removing headers as they were messing up height attribute for rect
+  // https://stackoverflow.com/questions/50902683/how-to-skip-or-ignore-rows-above-header
+  data = data.slice(1); 
+  
+  // Converting amount (y-values) from string to int 
+  data.forEach(d => +d.amount);
+  
+  // Using scaleband because it is categorical data 
+  // https://observablehq.com/@d3/d3-scaleband
+  const xScale = d3.scaleBand()
+    .domain(data.map((d) => d.category))
+    .range([50, 450])
+    .padding(0.5);
+
+  // From Scatter Plot
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.amount)])
+    .range([450, 50]);
+
+  //https://d3-graph-gallery.com/graph/interactivity_tooltip.html
+  const tooltip = d3.select("barchart")
+                    .append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 0);  
+
+  svg_Bar.selectAll("rect")
     .data(data)
     .enter()
-    .append("circle")
-        .attr("class", "circles")
-        .attr("id", (d) => {return "(" + xVal + ", " + yVal + ")"} )
-        .attr("cx", (d) => { return x(xVal) + MARGINS.left; } )
-        .attr("cy", (d) =>{ return y(yVal) + MARGINS.top; } )
-        .attr("r", 5)
+    .append("rect")
+    .attr("x", (d) => xScale(d.category))
+    .attr("y", (d) => yScale(d.amount))
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => 450 - yScale(d.amount))
+    .attr("fill", "#34baeb")
+    .on("mouseover", function(d) {
+      d3.select(this).style("fill", "#f5a742");
+    })
+    // change color when point is unhovered
+    .on("mouseout", function(d) {
+      d3.select(this).style("fill", "#34baeb");
     });
+
+  // https://d3-graph-gallery.com/intro_d3js.html
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  svg_Bar.append("g")
+    .attr("transform", "translate(0,450)")
+    .call(xAxis);
+
+  svg_Bar.append("g")
+    .attr("transform", "translate(50,0)")
+    .call(yAxis);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
